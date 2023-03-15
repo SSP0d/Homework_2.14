7f36fa86100dc3b5d73ebd007984232fa66e7453
Serhii Pidkopai <spodkopay@gmail.com>
2023-03-15 17:33:07 +0200

    config update

--- src/config/config.py
+++ src/config/config.py
@@ -2,19 +2,19 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    sqlalchemy_database_url: str
-    secret_key: str
-    algorithm: str
-    mail_username: str
-    mail_password: str
-    mail_from: str
-    mail_port: int
-    mail_server: str
-    redis_host: str
-    redis_port: int
-    cloudinary_name: str
-    cloudinary_api_key: str
-    cloudinary_api_secret: str
+    sqlalchemy_database_url: str = 'postgresql+psycopg2://user:password@localhost:5432/postgres'
+    secret_key: str = 'secret_key'
+    algorithm: str = 'HS256'
+    mail_username: str = 'example@ukr.net'
+    mail_password: str = 'secretPassword'
+    mail_from: str = 'example@ukr.net'
+    mail_port: int = 465
+    mail_server: str = 'smtp.ukr.net'
+    redis_host: str = "localhost"
+    redis_port: int = 6379
+    cloudinary_name: str = 'name'
+    cloudinary_api_key: int = 326488457974591
+    cloudinary_api_secret: str = 'secret'
 
     class Config:
         env_file = ".env"

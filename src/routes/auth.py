cffc4e3bc7dc93a7be3f54ed99d5a01a44d350b0
Serhii Pidkopai <spodkopay@gmail.com>
2023-03-15 10:35:23 +0200

    routes auth doc

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -13,6 +13,15 @@ security = HTTPBearer()
 
 @router.post("/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 async def signup(body: UserModel, db: Session = Depends(get_db)):
+    """
+    The signup function creates a new user in the database.
+        It takes a UserModel object as input and returns an HTTP response with the newly created user's information.
+        If there is already an account associated with that email address, it will return a 409 Conflict error.
+
+    :param body: UserModel: Define the type of data that will be received in the request body
+    :param db: Session: Get the database session
+    :return: A dictionary with the user and a detail message
+    """
     user_exist = await repository_users.get_user_by_email(body.email, db)
     if user_exist:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Account already exists")
@@ -23,6 +32,15 @@ async def signup(body: UserModel, db: Session = Depends(get_db)):
 
 @router.post("/login", response_model=TokenModel)
 async def login(body: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    """
+    The login function is used to authenticate a user.
+        It takes in the username and password of the user, and returns an access token if successful.
+        The access token can be used to make authenticated requests against protected endpoints.
+
+    :param body: OAuth2PasswordRequestForm: Get the username and password from the request body
+    :param db: Session: Get a database connection from the pool
+    :return: An access token and a refresh token
+    """
     user = await repository_users.get_user_by_email(body.username, db)
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email")
@@ -37,6 +55,16 @@ async def login(body: OAuth2PasswordRequestForm = Depends(), db: Session = Depen
 
 @router.get('/refresh_token', response_model=TokenModel)
 async def refresh_token(credentials: HTTPAuthorizationCredentials = Security(security), db: Session = Depends(get_db)):
+    """
+    The refresh_token function is used to refresh the access token.
+        The function takes in a refresh token and returns an access_token, a new refresh_token, and the type of token.
+        If there is no valid user associated with the given email address or if there are any errors during decoding
+        or creating tokens then an HTTPException will be raised.
+
+    :param credentials: HTTPAuthorizationCredentials: Get the token from the header
+    :param db: Session: Get a database session from the dependency injection container
+    :return: A dictionary with the access_token, refresh_token and token type
+    """
     token = credentials.credentials
     email = await auth_service.decode_refresh_token(token)
     user = await repository_users.get_user_by_email(email, db)
